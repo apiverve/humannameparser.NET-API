a3755b2308f28941f244a97a738610d927a51af8
APIVerve Team <hello@apiverve.com>
2025-01-18 12:02:25 -0600

    Bug Fixes

---
+++ HumanNameParserQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class HumanNameParserQueryOptions {
+public string name { get; set; }
+}
+
+}
